937d344a7a5b2bdb773eab30b1c0180dd186dc47
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-29 15:29:04 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const app = express()
-const PORT = 5000
+const path  = require('path')
 
+//set path to .env
+dotenv.config({path:'./.env'})
+
+const PORT = process.env.PORT || 4000;
+
+//Home page
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname + '/pages/home.html'))
 })
-
-app.get('/student',(req,res)=>{
-    res.send('Student Page')
+//About page
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/pages/about.html'))
 })
-
-app.get('/admin',(req,res)=>{
-    res.send('Admin Page')
+//Blog page
+app.get('/blog',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/pages/blog.html'))
 })
-
+//404 Page
+app.use((req,res,next)=>{
+    res.status(404)
+    res.sendFile(path.join(__dirname + '/pages/missing.html'))
+})
 
 app.listen(PORT,()=>{
     console.log(`Server running at http://localhost:${PORT}`)
